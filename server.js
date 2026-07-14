83a4ea31e0659fd540ab5d892ba6fde812ab21a5
sukanya79-o0 <sukanyareddy422@gmail.com>
2026-07-14 20:22:03 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
-const express = require("express");
+require("dotenv").config();
 
-const rideRoutes = require("./routes/rideRoutes");
-const userRoutes = require("./routes/userRoutes");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-app.use("/api/rides", rideRoutes);
-app.use("/api/users", userRoutes);
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected Successfully");
+})
+.catch((error) => {
+    console.log("MongoDB Connection Error:");
+    console.log(error.message);
+});
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+
+app.get("/", (req,res)=>{
+    res.send("Ucab Backend Running Successfully");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
 });
